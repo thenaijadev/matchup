7f8939c97cf59505cd698c6f3340a5b8cad7ce54
Okwor Chukwuemeka Nnajiofor <88171212+thenaijadev@users.noreply.github.com>
2024-07-19 15:54:37 +0100

    Feat: Search functionality for the activities

--- lib/features/search/presentation/search_screen_widgets/search_screen.dart
+++ lib/features/search/presentation/search_screen_widgets/search_screen.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:matchup/config/router/routes.dart';
 import 'package:matchup/core/widgets/input_field_widget.dart';
+import 'package:matchup/core/widgets/loading_widget.dart';
 import 'package:matchup/core/widgets/text_widget.dart';
+import 'package:matchup/features/activities/bloc/activities_bloc.dart';
+import 'package:matchup/features/activities/data/models/activities_model.dart';
 import 'package:matchup/features/auth/data/models/auth_user.dart';
 
-class SearchScreenWidget extends StatelessWidget {
+class SearchScreenWidget extends StatefulWidget {
   const SearchScreenWidget({super.key, required this.user});
   final AuthUser user;
+
+  @override
+  State<SearchScreenWidget> createState() => _SearchScreenWidgetState();
+}
+
+class _SearchScreenWidgetState extends State<SearchScreenWidget> {
+  String searchTerm = "";
+  List<Activities> activities = [];
+  List<Activities> filteredActivities = [];
+
+  @override
+  void initState() {
+    context.read<ActivitiesBloc>().add(ActivitiesEventGetAllActivities());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -20,34 +41,73 @@ class SearchScreenWidget extends StatelessWidget {
               ),
               hintColor: Theme.of(context).colorScheme.secondary,
               hintText: "Search for Activities",
-              onChanged: (val) {}),
+              onChanged: (val) {
+                setState(() {
+                  searchTerm = val ?? ""; // Handle empty string case
+                  filteredActivities = activities
+                      .where((activity) => activity.name!
+                          .toLowerCase()
+                          .contains(searchTerm.toLowerCase()))
+                      .toList(); // Filter based on search term (lowercase for case-insensitive)
+                });
+              }),
           const SizedBox(
             height: 10,
           ),
           Expanded(
-            child: ListView(
-              children: List.generate(
-                  2,
-                  (index) => GestureDetector(
-                        onTap: () =>
-                            Navigator.pushNamed(context, Routes.getDirections),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 15),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              TextWidget(
-                                text: "Indoor Friday Afternoon Games",
-                                color: Theme.of(context).colorScheme.secondary,
-                              ),
-                              Image.asset(
-                                "assets/images/go_icon.png",
-                                width: 10,
+            child: BlocConsumer<ActivitiesBloc, ActivitiesState>(
+              listener: (context, state) {
+                setState(() {
+                  if (state is ActivitiesStateAllActivitiesRetreived) {
+                    activities = state.activitiesModel.activities;
+                    filteredActivities =
+                        activities; // Update filtered list initially with all activities
+                  }
+                });
+              },
+              builder: (context, state) {
+                return BlocBuilder<ActivitiesBloc, ActivitiesState>(
+                  builder: (context, state) {
+                    return state is ActivitiesStateIsLoading
+                        ? const LoadingWidget()
+                        : state is ActivitiesStateAllActivitiesRetreived
+                            ? ListView(
+                                children: List.generate(
+                                    filteredActivities
+                                        .length, // Use filteredActivities for display
+                                    (index) => GestureDetector(
+                                          onTap: () => Navigator.pushNamed(
+                                              context, Routes.getDirections),
+                                          child: Padding(
+                                            padding: EdgeInsets.symmetric(
+                                                vertical: 15.h),
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .spaceBetween,
+                                              children: [
+                                                TextWidget(
+                                                  text:
+                                                      filteredActivities[index]
+                                                              .name ??
+                                                          "",
+                                                  color: Theme.of(context)
+                                                      .colorScheme
+                                                      .secondary,
+                                                ),
+                                                Image.asset(
+                                                  "assets/images/go_icon.png",
+                                                  width: 10.w,
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                        )),
                               )
-                            ],
-                          ),
-                        ),
-                      )),
+                            : const SizedBox();
+                  },
+                );
+              },
             ),
           )
         ],
